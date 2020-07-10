41059de02a61eb41974582c1bb7ddc89194f9e90
Sebastian <sebastiancbenjamin@gmail.com>
2020-07-09 17:17:58 -0700

    Implement banded alignment

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use std::env;
 use std::path;
 use bio::io::fasta;
 use bio::io::fastq;
-use bio::alignment::pairwise::*;
+use bio::alignment::pairwise::banded::*;
+use bio::alignment::sparse::hash_kmers;
 
 fn main() {
   const ALIGN_SCORE_THRESHOLD: usize = 60;
@@ -20,9 +21,11 @@ fn main() {
   let reverse_sequence = fastq::Reader::from_file(path::Path::new(&args[3])).unwrap();
 
   /* TODO: We will want to make the choice of global vs local vs semiglobal alignment depend on the reference library
-     For now, we're running the local alignment. */
+     For now, we're running the local banded alignment. */
   let score = |a: u8, b: u8| if a == b {1i32} else {-1i32};
-  let mut aligner = Aligner::new(-5, -1, &score);
+  let k = 8;  // kmer match length
+  let w = 6;  // Window size for creating the band
+  let mut aligner = Aligner::new(-5, -1, score, k, w);
 
   // Get sequences from the input files and align them
   // TODO: Make handle FASTQ.gz rather than having to uncompress manually first
@@ -33,21 +36,33 @@ fn main() {
 
   let reference_genome_size = 1210;//&reference_genome.size_hint().1;
   let mut current_reference_num = 1;
+  let mut curr_seq_num = 1;
 
   for reference in reference_genome {
     let reference = reference.unwrap();
-    let record = sequence.next();
-    let reverse_record = reverse_sequence.next();
+    //let reference_kmers_hash = hash_kmers(reference.seq(), k);
+    
+    for record in sequence.by_ref() {
+      if let Ok(seq) = record {
+        //print!("Sequence score: {}\n", aligner.semiglobal_with_prehash(seq.seq(), reference.seq(), &reference_kmers_hash).score)
+        print!("Sequence score: {}\n", aligner.local(seq.seq(), reference.seq()).score)
+      }
+      print!("{}\n", curr_seq_num);
+      curr_seq_num += 1;
+    }
+
+    curr_seq_num = 1;
+
+    for reverse_record in reverse_sequence.by_ref() {
+      if let Ok(seq) = reverse_record {
+        //print!("Reverse sequence score: {}\n", aligner.semiglobal_with_prehash(seq.seq(), reference.seq(), &reference_kmers_hash).score)
+        print!("Reverse sequence score: {}\n", aligner.local(seq.seq(), reference.seq()).score)
+      }
+      print!("{}\n", curr_seq_num);
+      curr_seq_num += 1;
+    }
 
     print!("Reference genome state: {:?}/{:?}\n", current_reference_num, reference_genome_size);
-    if let Some(seq) = record {
-      print!("Sequence score: {}\n", aligner.local(seq.unwrap().seq(), reference.seq()).score)
-    }
-
-    if let Some(seq) = reverse_record {
-      print!("Reverse sequence score: {}\n\n", aligner.local(seq.unwrap().seq(), reference.seq()).score)
-    }
-
     current_reference_num += 1;
   }
 }
