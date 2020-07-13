e096161db71ea0bd01193c923b37d3f3df18f690
Sebastian <sebastiancbenjamin@gmail.com>
2020-07-13 15:44:47 -0700

    Update comments

--- src/main.rs
+++ src/main.rs
@@ -17,21 +17,18 @@ fn main() {
   // TODO: Replace unwraps() with error handling where appropriate
   let reference_genome = fasta::Reader::from_file(path::Path::new(&args[1])).unwrap().records();
 
-  // Parallel map over the reference library
+  // Parallelized map over the reference library
   reference_genome.par_bridge().for_each(|reference| { 
     // Get the reference and its hash
     let reference = reference.unwrap();
     let reference_kmers_hash = hash_kmers(reference.seq(), K);
 
-    // Read the paired-end sequences that will be aligned
-    // TODO: Handle single-end sequences
+    // Get iterators to sequences that will be aligned from the sequence genome file(s)
+    // TODO: Handle single-end sequences, currently only does paired-end
     let sequence = fastq::Reader::from_file(path::Path::new(&args[2])).unwrap().records();
     let reverse_sequence = fastq::Reader::from_file(path::Path::new(&args[3])).unwrap().records();
 
     // Define aligner scoring parameters
-    /* TODO: We will want to make the choice of global vs local vs semiglobal alignment depend on the reference library
-     * For now, we're running the local banded alignment with hashing, configured with this scoring struct.
-     */
     const K: usize = 6; // kmer match length
     const W: usize = 20; // Window size for creating the band
     const MATCH: i32 = 1;
@@ -50,12 +47,20 @@ fn main() {
       yclip_suffix: 0,
     };
 
+    // Create banded aligner
+    /* TODO: We will want to make the choice of global vs local vs semiglobal alignment depend on the reference library
+     * For now, we're running the local banded alignment with hashing.
+     */
     let mut aligner = Aligner::with_scoring(scoring, K, W);
 
+    // Align each sequence and reverse sequence against current reference and return the scores
+    let mut seq_num = 1;
     for record in sequence {
+      seq_num += 1;
       if let Ok(seq) = record {
         let score = aligner.custom_with_prehash(seq.seq(), reference.seq(), &reference_kmers_hash).score;
-        print!("{}\n", score);
+        print!("score:{}\n", score);
+        print!("num:{}\n\n", seq_num);
       }
     }
 
