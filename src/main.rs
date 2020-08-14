1aa3c5b3f7a76930981baa3a986bac4f8fc91266
Sebastian <sebastiancbenjamin@gmail.com>
2020-08-13 18:35:54 -0700

    Replace unwraps with error handling

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod utils;
 
 use std::path;
 use std::fs::File;
-use std::io::Write;
+use std::io::{Write, Error};
 use std::collections::HashMap;
 use clap::{App, load_yaml};
 use bio::io::fasta;
@@ -13,14 +13,15 @@ use debruijn::dna_string::DnaString;
 type PseudoAligner = debruijn_mapping::pseudoaligner::Pseudoaligner<debruijn::kmer::VarIntKmer<u64, debruijn::kmer::K20>>;
 
 fn main() {
+  print!("Loading and preprocessing data\n");
   // Parse arguments based on the yaml schema
   let yaml = load_yaml!("cli.yml");
   let matches = App::from_yaml(yaml).get_matches();
 
-  let input_files: Vec<&str> = matches.values_of("input").unwrap().collect();
   let library = matches.value_of("library").unwrap();
   let library_fasta = matches.value_of("library_fasta").unwrap();
-  let num_cores = matches.value_of("num_cores").unwrap_or("1").parse::<usize>().expect("Please provide an integer value for the number of cores.");
+  let input_files: Vec<&str> = matches.values_of("input").unwrap().collect();
+  let num_cores = matches.value_of("num_cores").unwrap_or("1").parse::<usize>().expect("Error -- please provide an integer value for the number of cores");
 
   // Parameters for alignment
   const MATCH_THRESHOLD: usize = 60;
@@ -28,20 +29,33 @@ fn main() {
   const READS_SIZE: usize = 2786342;
 
   // Get iterators to records in the reference genome file and library
-  let reference_genome = fasta::Reader::from_file(library_fasta).unwrap().records();
-  let reference_library = utils::get_tsv_reader(File::open(path::Path::new(library)).unwrap());
+  let reference_genome: Vec<Result<fasta::Record, Error>> = fasta::Reader::from_file(library_fasta).expect(
+    "Error -- could not read reference genome"
+  ).records().collect();
 
-  // Load reference genome and library into memory
+  let mut reference_library = utils::get_tsv_reader(File::open(path::Path::new(library)).expect(
+    "Error -- could not read reference library"
+  ));
+  let mut reference_library = reference_library.records();
+
+  // Load reference genome and library into memory. Skip and log references that cannot be read.
   let mut reference_seqs = Vec::new();
-  for reference in reference_genome {
-    let reference = reference.unwrap();
-    reference_seqs.push(DnaString::from_acgt_bytes(reference.seq())); // Convert raw data to DNAString
-  }
-  
   let mut reference_names = Vec::new();
-  for reference in reference_library.into_records() {
-    let reference = reference.unwrap();
-    reference_names.push(reference[0].to_string());
+  for (i, reference) in reference_genome.iter().enumerate() {
+    let reference_name = reference_library.next();
+
+    if let Ok(reference) = reference {
+      if let Some(Ok(record)) = reference_name {
+        reference_seqs.push(DnaString::from_acgt_bytes(reference.seq())); // Convert raw data to DNAString
+        reference_names.push(record[0].to_string());
+      } else {
+        print!("Warning: Could not read library name #{}\n", i);
+        continue;
+      }
+    } else {
+      print!("Warning: Could not read library reference #{}\n", i);
+      continue;
+    }
   }
 
   // Create debruijn-mapped index of the reference library
@@ -50,28 +64,45 @@ fn main() {
     &reference_names,
     &HashMap::new(),
     num_cores
-  ).unwrap();
+  ).expect("Error -- could not create pseudoaligner index of the reference library");
 
   // Get iterator to the sequences that will be aligned to the reference from the sequence genome file(s)
-  let sequences = fastq::Reader::from_file(path::Path::new(input_files[0])).unwrap().records().map(|record| DnaString::from_acgt_bytes(record.unwrap().seq()));
-  let mut reverse_sequences = None;
+  // If a given sequence can be read, convert it to a DnaString. Otherwise, report an error.
+  let sequences = fastq::Reader::from_file(path::Path::new(input_files[0]))
+    .expect("Error -- cannot read sequence file: ")
+    .records()
+    .map(|record| match record { 
+      Ok(rec) => Ok(DnaString::from_acgt_bytes(rec.seq())),
+      _ => Err("Unable to read sequence")
+    });
 
-  if input_files.len() > 1 {
-    reverse_sequences = Some(fastq::Reader::from_file(path::Path::new(input_files[1])).unwrap().records().map(|record| DnaString::from_acgt_bytes(record.unwrap().seq())));
-  }
+  let mut reverse_sequences = if input_files.len() > 1 {
+    Some(fastq::Reader::from_file(path::Path::new(input_files[1]))
+          .expect("Error -- cannot read reverse sequence file: ")
+          .records()
+          .map(|record| match record { 
+            Ok(rec) => Ok(DnaString::from_acgt_bytes(rec.seq())),
+            _ => Err("Unable to read sequence") 
+          }))
+  } else {
+    None
+  };
 
+
+  print!("Pseudo-aligning reads to reference index\n");
   // Map over the sequence iterator, producing an iterator of score vectors for each sequence
   let reference_scores = sequences.fold(vec![0.0; REFERENCE_GENOME_SIZE], 
     |mut acc, read| {
 
       /* Generate score and equivalence class for this read by aligning the sequence against
        * the current reference. This alignment returns any scores that are greater than the match threshold. */
-      let seq_score = pseduoalign(&read, &reference_index, MATCH_THRESHOLD);
+      let seq_score = pseduoalign(read, &reference_index, MATCH_THRESHOLD);
       let mut rev_seq_score = None;
 
       // If there's a reversed sequence, do the paired-end alignment
       if let Some(itr) = &mut reverse_sequences {
-        rev_seq_score = Some(pseduoalign(&itr.next().unwrap(), &reference_index, MATCH_THRESHOLD));
+        let reverse_read = itr.next().expect("Error -- read and reverse read files do not have matching lengths: ");
+        rev_seq_score = Some(pseduoalign(reverse_read, &reference_index, MATCH_THRESHOLD));
       }
 
       // Get the score and the associated equivalence class
@@ -104,9 +135,16 @@ fn main() {
   let reference_scores: Vec<f32> = reference_scores.iter().map(|score| (score / READS_SIZE as f32) * 100.0).collect();
 
   // Create reference library iterator and filter results by lineage.
-  let reference_library = utils::get_tsv_reader(File::open(path::Path::new(library)).unwrap());
+  let reference_library = utils::get_tsv_reader(File::open(path::Path::new(library)).expect(
+    "Error -- could not read reference library"
+  ));
+
+
+  print!("Filtering results by lineage\n");
   let results = filter::collapse_results_by_lineage(reference_library.into_records(), reference_scores.iter());
 
+
+  print!("Writing results to file\n");
   // Write filtered results to file
   let mut str_rep = String::new();
 
@@ -123,13 +161,19 @@ fn main() {
     }
   }
 
-  let mut file = File::create("results.tsv").unwrap();
-  file.write_all(str_rep.as_bytes()).unwrap();
+  let mut file = File::create("results.tsv").expect("Error -- could not create results file");
+  file.write_all(str_rep.as_bytes()).expect("Error -- could not write results to file");
+
+  print!("Output results written to ./results.tsv")
 }
 
 // Align the given sequence against the given reference with a score threshold
-fn pseduoalign(sequence: &DnaString, reference_index: &PseudoAligner, match_threshold: usize) -> Option<(Vec<u32>, usize)> {
-  match reference_index.map_read(&sequence) {
+fn pseduoalign(sequence: Result<DnaString, &str>, reference_index: &PseudoAligner, match_threshold: usize) -> Option<(Vec<u32>, usize)> {
+  if sequence.is_err() {
+    return None;
+  }
+
+  match reference_index.map_read(&sequence.unwrap()) {
     Some((equiv_class, score)) => if score >= match_threshold && !equiv_class.is_empty() { Some((equiv_class, score)) } else { None }
     None => None
   }
