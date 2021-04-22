d6a843a6572867861986df803d4c56fba967ee6a
hextraza <sebastiancbenjamin@gmail.com>
2021-04-22 16:42:52 -0700

    Running forward integration tests with data files

--- tests/basic-cases.rs
+++ tests/basic-cases.rs
@@ -4,29 +4,37 @@ extern crate debruijn;
 extern crate debruijn_mapping;
 extern crate nimble;
 
-use debruijn::dna_string::DnaString;
-use nimble::align;
-use nimble::align::IntersectLevel;
-use nimble::reference_library;
-use nimble::utils::{validate_reference_pairs, DNAStringIter};
+use std::path::PathBuf;
 use std::collections::HashMap;
-use std::io::Error;
+use nimble::align;
+use nimble::reference_library;
+use nimble::utils;
 
 // Shared function for generating basic single strand test data
 fn get_basic_single_strand_data(
     reverse_comp_ref: bool,
 ) -> (
-    (DNAStringIter, DNAStringIter),
+    (utils::DNAStringIter, utils::DNAStringIter),
     (align::PseudoAligner, align::PseudoAligner),
     reference_library::ReferenceMetadata,
+    align::AlignFilterConfig
 ) {
-    let data_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).push("tests/test-sequences");
-    let basic_cases = data_path.clone().push("/libraries/basic.fasta");
-    let basic_cases_reverse = data_path.clone().push("/libraries/basic-rev.fasta");
-    let sequences = data_path.clone().push("/libraries/basic.fastq");
+    let mut data_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    data_path.push("tests/test-sequences");
+
+    let mut library = data_path.clone();
+
+    if reverse_comp_ref {
+        library.push("libraries/basic-rev.json");
+    } else {
+        library.push("libraries/basic.json");
+    }
+
+    let mut sequences = data_path.clone();
+    sequences.push("reads/basic.fastq");
     
     let (align_config, reference_metadata) =
-        reference_library::get_reference_library(Path::new(reference_json_path));
+        reference_library::get_reference_library(library.as_path());
 
     let (reference_seqs, reference_seqs_rev, reference_names) =
         utils::validate_reference_pairs(&reference_metadata);
@@ -36,92 +44,37 @@ fn get_basic_single_strand_data(
             &reference_seqs,
             &reference_names,
             &HashMap::new(),
-            num_cores,
+            1,
         )
-        .expect("Error -- could not create pseudoaligner index of the reference library");
+        .expect("Error -- could not create pseudoaligner index of the unit test reference library");
 
     let reference_index_reverse =
         debruijn_mapping::build_index::build_index::<debruijn_mapping::config::KmerType>(
             &reference_seqs_rev,
             &reference_names,
             &HashMap::new(),
-            num_cores,
+            1,
         )
-        .expect("Error -- could not create reverse pseudoaligner index of the reference library");
+        .expect("Error -- could not create reverse pseudoaligner index of the unit test reference library");
 
     let reference_index = (reference_index_forward, reference_index_reverse);
 
-    let sequences = utils::get_error_checked_fastq_readers(input_files[0]);
+    let sequences = utils::get_error_checked_fastq_readers(
+        &sequences.into_os_string().into_string()
+                 .expect("Could not convert unit test sequence to OsStr slice."));
 
-    let reverse_sequences = if input_files.len() > 1 {
-        println!("Reading reverse sequences");
-        Some(utils::get_error_checked_fastq_readers(input_files[1]))
-    } else {
-        None
-    };
-    
-
-    /* 'A02' is a portion of a the macaque MHC sequence Mamu-A1*002. A02-1 and A02-2 are 1bp and 2bp changes form A02 (see lower-case bases).
-    A02-LC is the same sequence as A02, just with some upper -> lower case changes to ensure that our results are case-insensitive. */
-    let mut columns: Vec<Vec<String>> = vec![
-        vec!["test", "test", "test"],
-        vec!["A02-0", "A02-1", "A02-2", "A02-LC", "KIR2DL-4"],
-        vec!["180", "180", "180"],
-    ]
-    .into_iter()
-    .map(|column| column.into_iter().map(|val| val.to_string()).collect())
-    .collect();
-    columns.push(reference_sequences);
-
-    let sequences: Vec<Result<DnaString, Error>> = sequences
-        .into_iter()
-        .map(|seq| Ok(DnaString::from_dna_string(&seq)))
-        .collect();
-
-    let reference_metadata = reference_library::ReferenceMetadata {
-        group_on: 1,
-        headers: vec![
-            "reference_genome",
-            "sequence_name_idx",
-            "nt_length",
-            "sequence_idx",
-        ]
-        .into_iter()
-        .map(|header| header.to_string())
-        .collect(),
-        columns,
-        sequence_name_idx: 1,
-        sequence_idx: 3,
-    };
-
-    let (reference_seqs, reference_seqs_rev, reference_names) =
-        validate_reference_pairs(&reference_metadata);
-
-    let reference_index_forward = debruijn_mapping::build_index::build_index::<
-        debruijn_mapping::config::KmerType,
-    >(&reference_seqs, &reference_names, &HashMap::new(), 1)
-    .expect("Error -- could not create pseudoaligner index of the reference library");
-
-    let reference_index_reverse = debruijn_mapping::build_index::build_index::<
-        debruijn_mapping::config::KmerType,
-    >(&reference_seqs_rev, &reference_names, &HashMap::new(), 1)
-    .expect("Error -- could not create pseudoaligner index of the reference library");
-
-    let reference_index = (reference_index_forward, reference_index_reverse);
-
-    (cast_to_iter_pair(sequences), reference_index, reference_metadata)
+    (sequences, reference_index, reference_metadata, align_config)
 }
 
-fn cast_to_iter_pair(sequences: Vec<Result<DnaString, Error>>) -> (DNAStringIter, DNAStringIter) {
-  (sequences.into_iter(), sequences.into_iter())
-}
-
-fn get_group_by_data() -> (
-    (DNAStringIter, DNAStringIter),
+fn get_group_by_data(
+    reverse_comp_ref: bool
+) -> (
+    (utils::DNAStringIter, utils::DNAStringIter),
     (align::PseudoAligner, align::PseudoAligner),
     reference_library::ReferenceMetadata,
+    align::AlignFilterConfig
 ) {
-    let (sequences, reference_index, mut reference_metadata) = get_basic_single_strand_data(false);
+    let (sequences, reference_index, mut reference_metadata, align_config) = get_basic_single_strand_data(reverse_comp_ref);
 
     reference_metadata.group_on = 4;
     reference_metadata.headers.push("test_group_on".to_string());
@@ -132,7 +85,7 @@ fn get_group_by_data() -> (
             .collect(),
     );
 
-    (sequences, reference_index, reference_metadata)
+    (sequences, reference_index, reference_metadata, align_config)
 }
 
 fn sort_score_vector(mut scores: Vec<(Vec<String>, i32)>) -> Vec<(Vec<String>, i32)> {
@@ -143,20 +96,7 @@ fn sort_score_vector(mut scores: Vec<(Vec<String>, i32)>) -> Vec<(Vec<String>, i
 #[test]
 // Case with zero mismatches
 fn basic_single_strand_no_mismatch_forward() {
-    let (sequences, reference_index, reference_metadata) = get_basic_single_strand_data(false);
-
-    // Configure aligner
-    let align_config = nimble::align::AlignFilterConfig {
-        reference_genome_size: 5,
-        score_threshold: 60,
-        num_mismatches: 0,
-        discard_nonzero_mismatch: false,
-        discard_multiple_matches: false,
-        score_filter: 0,
-        require_valid_pair: false,
-        intersect_level: IntersectLevel::NoIntersect,
-        discard_multi_hits: 0,
-    };
+    let (sequences, reference_index, reference_metadata, align_config) = get_basic_single_strand_data(false);
 
     let results = nimble::align::score(
         sequences,
@@ -188,20 +128,9 @@ fn basic_single_strand_no_mismatch_forward() {
 #[test]
 // Case with one mismatch
 fn basic_single_strand_one_mismatch_forward() {
-    let (sequences, reference_index, reference_metadata) = get_basic_single_strand_data(false);
+    let (sequences, reference_index, reference_metadata, mut align_config) = get_basic_single_strand_data(false);
 
-    // Configure aligner
-    let align_config = nimble::align::AlignFilterConfig {
-        reference_genome_size: 5,
-        score_threshold: 60,
-        num_mismatches: 1,
-        discard_nonzero_mismatch: false,
-        discard_multiple_matches: false,
-        score_filter: 0,
-        require_valid_pair: false,
-        intersect_level: IntersectLevel::NoIntersect,
-        discard_multi_hits: 0,
-    };
+    align_config.num_mismatches = 1;
 
     let results = nimble::align::score(
         sequences,
@@ -233,20 +162,9 @@ fn basic_single_strand_one_mismatch_forward() {
 #[test]
 // Case with two mismatches
 fn basic_single_strand_two_mismatch_forward() {
-    let (sequences, reference_index, reference_metadata) = get_basic_single_strand_data(false);
+    let (sequences, reference_index, reference_metadata, mut align_config) = get_basic_single_strand_data(false);
 
-    // Configure aligner
-    let align_config = nimble::align::AlignFilterConfig {
-        reference_genome_size: 5,
-        score_threshold: 60,
-        num_mismatches: 2,
-        discard_nonzero_mismatch: false,
-        discard_multiple_matches: false,
-        score_filter: 0,
-        require_valid_pair: false,
-        intersect_level: IntersectLevel::NoIntersect,
-        discard_multi_hits: 0,
-    };
+    align_config.num_mismatches = 2;
 
     let results = nimble::align::score(
         sequences,
@@ -278,20 +196,7 @@ fn basic_single_strand_two_mismatch_forward() {
 #[test]
 // Case with zero mismatches
 fn basic_single_strand_no_mismatch_reverse() {
-    let (sequences, reference_index, reference_metadata) = get_basic_single_strand_data(true);
-
-    // Configure aligner
-    let align_config = nimble::align::AlignFilterConfig {
-        reference_genome_size: 5,
-        score_threshold: 60,
-        num_mismatches: 0,
-        discard_nonzero_mismatch: false,
-        discard_multiple_matches: false,
-        score_filter: 0,
-        require_valid_pair: false,
-        intersect_level: IntersectLevel::NoIntersect,
-        discard_multi_hits: 0,
-    };
+    let (sequences, reference_index, reference_metadata, align_config) = get_basic_single_strand_data(true);
 
     let results = nimble::align::score(
         sequences,
@@ -323,20 +228,9 @@ fn basic_single_strand_no_mismatch_reverse() {
 #[test]
 // Case with one mismatch
 fn basic_single_strand_one_mismatch_reverse() {
-    let (sequences, reference_index, reference_metadata) = get_basic_single_strand_data(true);
+    let (sequences, reference_index, reference_metadata, mut align_config) = get_basic_single_strand_data(true);
 
-    // Configure aligner
-    let align_config = nimble::align::AlignFilterConfig {
-        reference_genome_size: 5,
-        score_threshold: 60,
-        num_mismatches: 1,
-        discard_nonzero_mismatch: false,
-        discard_multiple_matches: false,
-        score_filter: 0,
-        require_valid_pair: false,
-        intersect_level: IntersectLevel::NoIntersect,
-        discard_multi_hits: 0,
-    };
+    align_config.num_mismatches = 1;
 
     let results = nimble::align::score(
         sequences,
@@ -368,20 +262,9 @@ fn basic_single_strand_one_mismatch_reverse() {
 #[test]
 // Case with two mismatches
 fn basic_single_strand_two_mismatch_reverse() {
-    let (sequences, reference_index, reference_metadata) = get_basic_single_strand_data(true);
+    let (sequences, reference_index, reference_metadata, mut align_config) = get_basic_single_strand_data(true);
 
-    // Configure aligner
-    let align_config = nimble::align::AlignFilterConfig {
-        reference_genome_size: 5,
-        score_threshold: 60,
-        num_mismatches: 2,
-        discard_nonzero_mismatch: false,
-        discard_multiple_matches: false,
-        score_filter: 0,
-        require_valid_pair: false,
-        intersect_level: IntersectLevel::NoIntersect,
-        discard_multi_hits: 0,
-    };
+    align_config.num_mismatches = 2;
 
     let results = nimble::align::score(
         sequences,
@@ -411,22 +294,33 @@ fn basic_single_strand_two_mismatch_reverse() {
 }
 
 #[test]
-// Case with group_by instead of basic allele-level reporting
-fn group_by() {
-    let (sequences, reference_index, reference_metadata) = get_group_by_data();
+// Case with group_by instead of basic forward allele-level reporting
+fn group_by_forward() {
+    let (sequences, reference_index, reference_metadata, align_config) = get_group_by_data(false);
 
-    // Configure aligner
-    let align_config = nimble::align::AlignFilterConfig {
-        reference_genome_size: 5,
-        score_threshold: 60,
-        num_mismatches: 0,
-        discard_nonzero_mismatch: false,
-        discard_multiple_matches: false,
-        score_filter: 0,
-        require_valid_pair: false,
-        intersect_level: IntersectLevel::NoIntersect,
-        discard_multi_hits: 0,
-    };
+    let results = nimble::align::score(
+        sequences,
+        None,
+        reference_index,
+        &reference_metadata,
+        &align_config,
+    );
+    let results = sort_score_vector(results);
+
+    let expected_results = vec![
+        (vec![String::from("g1")], 1),
+        (vec![String::from("g1"), String::from("g2")], 1),
+        (vec![String::from("g2")], 2),
+    ];
+    let expected_results = sort_score_vector(expected_results);
+
+    assert_eq!(results, expected_results);
+}
+
+#[test]
+// Case with group_by instead of basic reverse allele-level reporting
+fn group_by_rev() {
+    let (sequences, reference_index, reference_metadata, align_config) = get_group_by_data(true);
 
     let results = nimble::align::score(
         sequences,
