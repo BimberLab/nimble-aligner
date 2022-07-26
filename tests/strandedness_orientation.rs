f9faef77668f8e918817510bd01cd161f4c6b92d
hextraza <sebastiancbenjamin@gmail.com>
2022-07-26 12:17:20 -0700

    Test revcomp failure

--- tests/strandedness_orientation.rs
+++ tests/strandedness_orientation.rs
@@ -404,6 +404,45 @@ fn fiveprime_filter_uf_failure() {
 }
 
 #[test]
+fn fiveprime_filter_ur_revcomp_failure() {
+    // SETUP
+    let seq_filename = "UR_reversecomp.sam";
+    let lib_filename = "strandedness.json";
+    let (_, reference_index, reference_metadata, align_config) = utils::get_data(seq_filename, lib_filename, nimble::align::StrandFilter::FivePrime);
+
+    let mut data_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    data_path.push("tests/test-sequences");
+
+    let mut sequences = data_path.clone();
+    sequences.push("reads/");
+    sequences.push(seq_filename);
+
+    let sequences = &sequences
+            .into_os_string()
+            .into_string()
+            .expect("Could not convert unit test sequence to OsStr slice.");
+
+
+    // TEST
+    let results = nimble::process::bam::process(
+        vec![sequences],
+        &reference_index,
+        &reference_metadata,
+        &align_config,
+        "/dev/null",
+        None,
+        None
+    );
+
+    let results = utils::sort_score_vector(results);
+
+    let expected_results = vec![];
+    let expected_results = utils::sort_score_vector(expected_results);
+
+    assert_eq!(results, expected_results);
+}
+
+#[test]
 fn fiveprime_filter_fr_success() {
     // SETUP
     let seq_filename = "strandedness_FR.sam";
